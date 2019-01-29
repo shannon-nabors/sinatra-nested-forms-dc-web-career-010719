7b8bb7bd61f19dd2bb233ba02386def70de55165
shannon-nabors <shannon.nabors@gmail.com>
2019-01-28 21:30:10 -0500

    done

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+
+  attr_reader :name, :height, :weight
+  @@all = []
+
+  def initialize(params)
+    @name, @weight, @height = params[:name], params[:weight], params[:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+
+  attr_reader :name, :type, :booty
+  @@all = []
+
+  def initialize(args)
+    @name, @type, @booty = args[:name], args[:type], args[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+
 end
